f01a02eaf938996b2602408a14d115ef05d7bbe7
Zheng Cao <chori84@gmail.com>
2018-04-18 10:19:25 -0700

    Testing Authentication Step 4: test existing user

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,21 @@
 require "test_helper"
 
 describe SessionsController do
-  it "should get create" do
-    get sessions_create_url
-    value(response).must_be :success?
-  end
 
-  it "should get destroy" do
-    get sessions_destroy_url
-    value(response).must_be :success?
+  describe "create" do
+    it "should log in an existing user and redirect to the root route" do
+      before_count = User.count
+      existing_user = users(:ada)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(existing_user))
+
+      proc {
+        get auth_callback_path(existing_user.provider)
+      }.must_change 'User.count', 0
+
+      must_redirect_to root_path
+      User.count.must_equal before_count
+    end
   end
 
 end
