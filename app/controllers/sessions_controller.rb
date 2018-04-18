66c6ad83e1683baf978eb646e79c6b173a34b3ad
Zheng Cao <chori84@gmail.com>
2018-04-18 09:34:29 -0700

    Minor code format changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,33 +4,35 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth_hash = request.env['omniauth.auth']
+      auth_hash = request.env['omniauth.auth']
 
-    if auth_hash[:uid]
-      @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
-      # @user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
+      if auth_hash[:uid]
+        @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+        # @user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
 
-      if @user.nil?
-        @user = User.build_from_github(auth_hash)
-        successful_save = @user.save
-        if successful_save
+        if @user.nil?
+          @user = User.build_from_github(auth_hash)
+          successful_save = @user.save
+
+          if successful_save
+            flash[:success] = "Logged in successfully"
+            session[:user_id] = @user.id
+            redirect_to root_path
+          else
+            flash[:error] = "Some error happened in User creation"
+            redirect_to root_path
+          end
+          
+        else
           flash[:success] = "Logged in successfully"
           session[:user_id] = @user.id
           redirect_to root_path
-        else
-          flash[:error] = "Some error happened in User creation"
-          redirect_to root_path
         end
+
       else
-        flash[:success] = "Logged in successfully"
-        session[:user_id] = @user.id
+        flash[:error] = "Logging in through GitHub not successful"
         redirect_to root_path
       end
-
-    else
-      flash[:error] = "Logging in through GitHub not successful"
-      redirect_to root_path
-    end
 
     # @user = User.find_by(name: params[:user][:name])
     # if @user
